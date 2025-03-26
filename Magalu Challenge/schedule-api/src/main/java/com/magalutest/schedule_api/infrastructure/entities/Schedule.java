22135b2ae2ebb1e1a26cee7da3c100effa816fc7
Gabriel Mendes <biell.mendes8@gmail.com>
2025-03-26 14:48:31 -0300

    chore: add schedule entity

---
+++ Magalu Challenge/schedule-api/src/main/java/com/magalutest/schedule_api/infrastructure/entities/Schedule.java
@@ -0,0 +1,30 @@
+package com.magalutest.schedule_api.infrastructure.entities;
+
+import com.magalutest.schedule_api.infrastructure.enums.StatusNotificationEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "schedule")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String emailDestinatary;
+    private String phoneDestinatary;
+    private LocalDateTime dateHourSend;
+    private LocalDateTime dateHourSchedule;
+    private LocalDateTime dateHourModification;
+    private String mensage;
+    private StatusNotificationEnum statusNotificationEnum;
+}
