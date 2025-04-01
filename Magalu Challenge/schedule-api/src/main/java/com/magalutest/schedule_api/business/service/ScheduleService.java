2ea03e986cade4d18eed007ae85a39e9f0a1db8a
Gabriel Mendes <biell.mendes8@gmail.com>
2025-03-31 23:32:36 -0300

    log: add loggers in ScheduleService

--- Magalu Challenge/schedule-api/src/main/java/com/magalutest/schedule_api/business/service/ScheduleService.java
+++ Magalu Challenge/schedule-api/src/main/java/com/magalutest/schedule_api/business/service/ScheduleService.java
@@ -7,28 +7,36 @@ import com.magalutest.schedule_api.infrastructure.entities.Schedule;
 import com.magalutest.schedule_api.infrastructure.exception.NotFoundException;
 import com.magalutest.schedule_api.infrastructure.repositories.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
     private final ScheduleMapper scheduleMapper;
 
     public ScheduleRecordOut postSchedule(ScheduleRecordIn scheduleIn) {
+        log.info("[ScheduleService] postSchedule: executed -> " + scheduleIn);
+
         return scheduleMapper.toOut(
                 scheduleRepository.save(
                     scheduleMapper.toEntity(scheduleIn)));
     }
 
     public ScheduleRecordOut getScheduleById(Long id) {
+        log.info("[ScheduleService] getScheduleById: executed -> id: " + id);
+
         return scheduleMapper.toOut(
                 scheduleRepository.findById(id)
                         .orElseThrow(() -> new NotFoundException("Id Not Found")));
     }
 
     public void cancelSchedule(Long id) {
+        log.info("[ScheduleService] cancelSchedule: executed -> id: " + id);
+
         Schedule scheduleEntity =  scheduleRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Id Not Found"));
 
